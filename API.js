92164337b4603bef1253cbdbc52b3b57c58b220e
Mina Tawfik <minatawfik97@gmail.com>
2023-12-29 00:35:56 +0200

    added and tested JS and some design

--- API.js
+++ API.js
@@ -1,8 +1,51 @@
-async function getAPI(){
-    var response = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=eaa86ae5ef2157748f0b8021bb6ec5df&language=en")
-    var finalResponse = await response.json()
-    console.log(finalResponse);
+let cityInput = document.getElementById("location")
+
+document.querySelector("#location")?.addEventListener("keyup", function(){
+    getWeatherFor3Days(cityInput.value)
+})
+
+async function getWeatherFor3Days(city){
+    let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=e89e30175c1348d0927184930232812&q=${city}&days=2`)
+    let finalResponse = await response.json()
+    showCurrent(finalResponse)
+}
+
+function showCurrent(allInfo){
+    let showHTML = ""
+    showHTML += `
+    <h4 class="w-100 text-center">${allInfo.location.name}</h4>
+    `
+    document.getElementById("current").innerHTML = showHTML
 }
 
 
-getAPI()
+
+// function showDays(allInfo){
+//     for (let i = 0 ; i < getWeatherFor3Days(city).forecast.forecastday.length ; i++){
+//         showHTML += `
+    
+//         `
+//     }
+// }
+
+
+
+
+// getWeatherFor3Days(city)
+
+
+// location.name
+
+// current.temp_c
+// current.condition.icon
+// current.condition.text
+
+// current.wind_kph
+// -------------
+// forecast.forecastday[i].date
+// forecast.forecastday[i].day.condition.mintemp_c
+// forecast.forecastday[i].day.condition.maxtemp_c
+// forecast.forecastday[i].day.condition.icon
+// forecast.forecastday[i].day.condition.text
+
+// forecast.forecastday[i].day.condition.maxwind_kph
